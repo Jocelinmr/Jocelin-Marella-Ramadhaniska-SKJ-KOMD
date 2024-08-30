3a150e33fd60e9338348823ea84f7922c0dca2ea
Jocelinmr <159984783+Jocelinmr@users.noreply.github.com>
2024-08-30 15:44:36 +0700

    Update perkalian.cpp

--- Assignment1/Task2/perkalian.cpp
+++ Assignment1/Task2/perkalian.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 10;
+    int result;
+
+    result = num1 * num2;
+
+    cout << result << endl;
+
+    return 0;
+}
